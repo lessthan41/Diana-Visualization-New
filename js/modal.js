1f50100edfb454fd954f68ef44eaeab3e76bebe2
Wayne Ho <43234148+lessthan41@users.noreply.github.com>
2020-04-16 20:51:34 +0800

    detail table first try

--- js/modal.js
+++ js/modal.js
@@ -38,8 +38,8 @@ function buildingModal(data, userid) {
 
 
 function detailModal(data, building_name) {
-    console.log(data);
-    console.log(geojson);
+    // console.log(data);
+    // console.log(geojson);
     var dialog2 = document.querySelector('#detail-modal');
     dialog2.showModal();
 
@@ -53,7 +53,7 @@ function detailModal(data, building_name) {
 
     var problemNumbers = Object.keys(data);
     var one, two, three, color, severity;
-    for (var i in Object.keys(data)) {
+    for (var i in data) {
         one = data[i]['question'];
         two = data[i]['content'];
         three = data[i]['description'];
@@ -65,8 +65,9 @@ function detailModal(data, building_name) {
         var tr = $('#modal-detail-table tbody').append($('<tr />').addClass(color)
             .append($('<td />').addClass('mdl-data-table__cell--non-numeric').html(one))
             .append($('<td />').html(two).attr('style', 'text-align: center;'))
-            .append($('<td />').html(three))
-            .append($('<td />').html(four).attr('style', 'text-align: center;'))
+            .append($('<td />').html(three).attr('style', 'text-align: center;'))
+            .append($('<td />').append($('<a />').attr('href', four).attr('target', '_blank').html('link'))
+                .attr('style', 'text-align: center;'))
             .append($('<td />').html(five).attr('style', 'text-align: center;')));
     }
 }
